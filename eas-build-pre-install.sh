e36f7e567d999be00cf125e62a1c4b95fd7dd1f9
Jose Mokeni <jmmokeni@gmail.com>
2025-05-15 14:50:34 +0100

    eas script update

--- eas-build-pre-install.sh
+++ eas-build-pre-install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Decode the base64 environment variable to google-services.json
-echo "$GOOGLE_SERVICES_JSON_BASE64" | tr -d '\n' | base64 -d > google-services.json
+# Decode the base64 environment variable to google-services.json, removing spaces and newlines
+echo "$GOOGLE_SERVICES_JSON_BASE64" | tr -d '\n ' | base64 -d > google-services.json
 
 echo "Created google-services.json"
